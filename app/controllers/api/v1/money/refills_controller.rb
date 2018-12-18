63958d56164460d97086b7c5ffee53b4aea67700
ivan.filenko <filenko@uchi.ru>
2018-12-18 21:25:52 +0300

    Implement Money::Refill operation

--- app/controllers/api/v1/money/refills_controller.rb
+++ app/controllers/api/v1/money/refills_controller.rb
@@ -1,5 +1,11 @@
 class Money::RefillsController < ApplicationController
   def create
+    result = Money::Refill.new(params).call
 
+    if result.successful?
+      head :ok
+    else
+      render json: { error: result.error }, status: :unprocessable_entity
+    end
   end
 end
--- app/operations/money/refill.rb
+++ app/operations/money/refill.rb
@@ -1,9 +1,30 @@
 class Money::Refill
-  def initialize(params)
-    @params = params
+  attr_reader :error
+
+  def initialize
+    @error   = nil
+    @success = nil
   end
 
-  def call
-    # refill lofic here
+  def call(params)
+    Money.transaction do
+      @params.each do |par_value, count|
+        money = Money.find_by!(value: par_value)
+        money.lock!
+        money.count += count
+        money.save!
+      end
+    end
+
+    @success = true
+  rescue StandardError => e
+    @success = false
+    @error   = e.message
+  ensure
+    return self
+  end
+
+  def successful?
+    @success
   end
 end

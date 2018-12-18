a18c72e610da4952bfefc28c8cf6c579524a428f
ivan.filenko <filenko@uchi.ru>
2018-12-18 20:56:46 +0300

    Add stubs for money refill & withdraw operations

---
+++ app/operations/money/refill.rb
@@ -0,0 +1,9 @@
+class Money::Refill
+  def initialize(params)
+    @params = params
+  end
+
+  def call
+    # refill lofic here
+  end
+end
---
+++ app/operations/money/withdraw.rb
@@ -0,0 +1,9 @@
+class Money::Withdraw
+  def initialize(total)
+    @total = total
+  end
+
+  def call
+    # withdraw logic
+  end
+end

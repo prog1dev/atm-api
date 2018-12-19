b9dd47434beb4aaf4c941560ef34f1002fb067f1
ivan.filenko <filenko@uchi.ru>
2018-12-19 18:25:23 +0300

    Create Money factory

---
+++ spec/factories/money.rb
@@ -0,0 +1,6 @@
+FactoryBot.define do
+  factory :money do
+    value { 0 }
+    count { 0 }
+  end
+end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -8,7 +8,7 @@ ActiveRecord::Migration.maintain_test_schema!
 RSpec.configure do |config|
   config.use_transactional_fixtures = true
   # config.infer_spec_type_from_file_location!
-  # config.include FactoryGirl::Syntax::Methods
+  config.include FactoryBot::Syntax::Methods
   config.expect_with :rspec do |c|
     c.syntax = :expect
   end

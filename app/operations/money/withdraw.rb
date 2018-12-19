e764e1a73accc9962eacf4a62635dbf5f83821c5
ivan.filenko <filenko@uchi.ru>
2018-12-19 18:07:26 +0300

    Implement Money::Withdraw operation

--- app/operations/money/withdraw.rb
+++ app/operations/money/withdraw.rb
@@ -7,9 +7,31 @@ class Money::Withdraw
   end
 
   def call(total)
-    # withdraw logic
+    Money.transaction do
+      available_bills = Money.pluck(:value, :count).to_h.sort_by { |k, v| -k }.to_h
+      result = {}
+
+      available_bills.each do |denomination, count|
+        available_bills_count = total / denomination
+
+        bills = available_bills_count < count ? available_bills_count : count
+        result[denomination] = bills
+        total -= bills * denomination
+      end
+
+      raise 'Not enough bills available' if total > 0
+
+      result.each do |k, v|
+        money = Money.find_by!(value: k)
+        money.count -= v
+        money.save!
+      end
+    end
 
     @success = true
+  rescue PG::CheckViolation => e
+    @success = false
+    @error   = 'Not enough bills available'
   rescue StandardError => e
     @success = false
     @error   = e.message
